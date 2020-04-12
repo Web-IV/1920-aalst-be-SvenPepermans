180aaff1546fb883501d7337e289a0ae6eff5998
Sven <sven.pepermans@student.hogent.be>
2020-04-12 17:10:56 +0200

    Update Startup.cs

--- PicturePerfectAPI/PicturePerfectAPI/Startup.cs
+++ PicturePerfectAPI/PicturePerfectAPI/Startup.cs
@@ -7,7 +7,9 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -15,6 +17,9 @@ using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using NSwag;
 using NSwag.Generation.Processors.Security;
+using PicturePerfectAPI.Data;
+using PicturePerfectAPI.Data.Repositories;
+using PicturePerfectAPI.Models;
 
 namespace PicturePerfectAPI
 {
@@ -31,6 +36,39 @@ namespace PicturePerfectAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<ApplicationDbContext>(options =>
+               options.UseSqlServer(
+                   Configuration.GetConnectionString("DefaultConnection")));
+
+
+            services.AddScoped<PostsDataInitializer>();
+            services.AddScoped<IPostRepository, PostRepository>();
+            services.AddScoped<IGebruikerRepository, GebruikerRepository>();
+            services.AddScoped<IFotoRepository, FotoRepository>();
+
+            services.AddIdentity<IdentityUser, IdentityRole>(cfg => cfg.User.RequireUniqueEmail = true).AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings.
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+
+                // Lockout settings.
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+
+                // User settings.
+                options.User.AllowedUserNameCharacters =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._";
+                options.User.RequireUniqueEmail = true;
+            });
+
             services.AddSwaggerDocument();
             services.AddAuthentication(x =>
             {
@@ -69,11 +107,13 @@ namespace PicturePerfectAPI
             });
             services.AddCors(options =>
                  options.AddPolicy("AllowAllOrigins", builder => builder.AllowAnyOrigin()));
+
+           
         }
 
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, PostsDataInitializer postsDataInitializer)
         {
             if (env.IsDevelopment())
             {
@@ -92,6 +132,8 @@ namespace PicturePerfectAPI
             {
                 endpoints.MapControllers();
             });
+
+            postsDataInitializer.InitializeDataAsync().Wait();
         }
     }
 }
